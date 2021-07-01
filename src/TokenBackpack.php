ddf18444eaf6d7af566b3d1c19ddb854aab159a2
nakatsuji masato <nakatsuji@teastalk.jp>
2021-07-01 13:32:00 +0900

    7/1 Add Readme.md

--- src/TokenBackpack.php
+++ src/TokenBackpack.php
@@ -1,5 +1,20 @@
 <?php
 
+/**
+ * ===================================================
+ * 
+ * PHP Framework "Mk2"
+ *
+ * TokenBackpack
+ * 
+ * URL : https://www.mk2-php.com/
+ * 
+ * Copylight : Nakajima-Satoru 2021.
+ *           : Sakaguchiya Co. Ltd. (https://www.teastalk.jp/)
+ * 
+ * ===================================================
+ */
+
 namespace mk2\backpack_token;
 
 use Mk2\Libraries\Backpack;
